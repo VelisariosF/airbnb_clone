8cc9c83c2598350cc7532a7ae7b523a64583a34c
Velisarios1234 <fafoutis@csd.auth.gr>
2022-04-13 16:05:06 +0300

    created navigation bar components

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,16 +1,24 @@
+import React from 'react'
 import NavbarExperiences from "./NavbarComponents/NavbarExperiences"
 import NavbarPlacesToStay from "./NavbarComponents/NavbarPlacesToStay"
 import ProfileModerator from "./NavbarComponents/ProfileModerator"
 export default function Navbar() {
+    const [displayPlacesToStayForm, setDisplayPlacesToStayForm] = React.useState(true)
+    const [displayExperiencesForm, setdisplayExperiencesForm] = React.useState(true)
+    
+
+    function handleFormsDisplay(id){
+        
+    }
     return (
         <nav className='main-nav'>
             <div className='logo'></div>
             <ul className='search-ul'>
                 <li className='places-to-stay'>
-                    <NavbarPlacesToStay />
+                    <NavbarPlacesToStay id={1} show={showPlacesToStay} />
                 </li>
                 <li className='experiences'>
-                    <NavbarExperiences />
+                    <NavbarExperiences id={2} show={choosePlacesToStay}/>
                 </li>
 
             </ul>
