393efc808581b5099f673baef00fc9b3b8fbe632
Velisarios1234 <fafoutis@csd.auth.gr>
2022-04-13 15:58:15 +0300

    created navigation bar components

--- src/NavbarComponents/NavbarExperiences.js
+++ src/NavbarComponents/NavbarExperiences.js
@@ -1,8 +1,56 @@
+import React from 'react'
 import './NavbarExperiences.css'
-export default function NavbarExperiences(){
+export default function NavbarExperiences(props) {
+  const formDisplayStyle ={
+     opacity: props.show ? 1 : 0
+  }
+  const [formData, setFormData] = React.useState({
+    location: '',
+    date: ''
+  })
+
+  function handleChange(event) {
+    const { name, value } = event.target
+    setFormData(prevFormData => {
+      return {
+        ...prevFormData,
+        [name]: value
+      }
+    })
+  }
+
+  function handleSubmit() {
+
+  }
+
+
   return (
-     <form>
-         
-     </form>
+    <div>
+      <h3 >Experiences</h3>
+      <form style={formDisplayStyle} onSubmit={handleSubmit}>
+        <div className='form-element'>
+          <label htmlFor='location'>location</label>
+          <input
+            type='text'
+            name='location'
+            placeholder='Where are you going?'
+            value={formData.location}
+            onChange={handleChange} />
+
+        </div>
+        <div className='form-element'>
+          <label htmlFor='date'>Date</label>
+          <input
+            type='date'
+            name='date'
+            placeholder='Add when you want to go'
+            value={formData.date}
+            onChange={handleChange} />
+        </div>
+
+        <button className='search-button' placeholder='Search'><i class="fa fa-search"><span>Search</span></i></button>
+
+      </form>
+    </div>
   )
 }
--- src/NavbarComponents/NavbarPlacesToStay.js
+++ src/NavbarComponents/NavbarPlacesToStay.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import GuestCounter from './GuestsFromExperiences/GuestCounter'
 import './NavbarPlacesToStay.css'
-export default function NavbarPlacesToStay() {
-    
+export default function NavbarPlacesToStay(props) {
+    const formDisplayStyle ={
+        opacity: props.show ? 1 : 0
+     }
     const [guestsCounters, setGuestsCounters] = React.useState(() => [0, 0, 0, 0])
     const [guestsSum, setGuestsSum] = React.useState(() => 0)
-    const [formData, setFormData] = React.useState(  {
+    const [formData, setFormData] = React.useState({
         location: "",
         checkIn: "",
         checkOut: "",
@@ -16,50 +18,59 @@ export default function NavbarPlacesToStay() {
 
     const guest_counter_elements = guestsCounters.map((value, index) => {
         return (
-           <GuestCounter 
-           value ={value} 
-           add ={() => add(index)}
-           subtract ={() => subtract(index)}/>
+            <GuestCounter
+                value={value}
+                add={() => add(index)}
+                subtract={() => subtract(index)} />
         )
     })
 
     const guest_counter_container_style = {
-        opacity : showGuestsContainer ? 1 : 0
+        opacity: showGuestsContainer ? 1 : 0
     }
 
-   
 
-    function updateGuestsSum(add){
+
+    function updateGuestsSum(add) {
         setFormData(prevFormData => {
-            let newFormData = {...prevFormData}
-            if(add)
-              newFormData.guests += 1
-            else 
-              newFormData.guests -= 1
-            
+            let newFormData = { ...prevFormData }
+            if (add) {
+                newFormData.guests += 1
+            }
+
+            else {
+                if (formData.guests !== 0) {
+                    newFormData.guests -= 1
+                }
+            }
+
+
             return newFormData
         })
-      /*  if(add){
-            setGuestsSum(guestsSum + 1)
-        }else{
-            setGuestsSum(guestsSum - 1)
-        }*/
-        
-       
+        /*  if(add){
+              setGuestsSum(guestsSum + 1)
+          }else{
+              setGuestsSum(guestsSum - 1)
+          }*/
+
+
     }
     function add(index) {
-       const countersCopy = [...guestsCounters];
-             countersCopy[index] += 1;
-             setGuestsCounters(countersCopy);
-             updateGuestsSum(true)
-   }
-   
-   function subtract(index) {
         const countersCopy = [...guestsCounters];
-             countersCopy[index] -= 1;
-             setGuestsCounters(countersCopy);
-             updateGuestsSum(false)
-   }
+        countersCopy[index] += 1;
+        setGuestsCounters(countersCopy);
+        updateGuestsSum(true)
+    }
+
+    function subtract(index) {
+        if (formData.guests !== 0) {
+            const countersCopy = [...guestsCounters];
+            countersCopy[index] -= 1;
+            setGuestsCounters(countersCopy);
+            updateGuestsSum(false)
+        }
+
+    }
 
 
     function handelSubmit() {
@@ -67,63 +78,67 @@ export default function NavbarPlacesToStay() {
     }
 
     function handleChange(event) {
-        const {name, value, type} = event.target
+        const { name, value } = event.target
         setFormData(prevFormData => {
             return {
                 ...prevFormData,
-                [name] : value
+                [name]: value
             }
         })
     }
     return (
-       <div className='places-to-stay-form'>
-        <form onSubmit={handelSubmit}>
-            <div className='form-element'>
-                <label htmlFor='location'>location</label>
-                <input
-                    type='text'
-                    name='location'
-                    placeholder='Where are you going?'
-                    value={formData.location}
-                    onChange={handleChange} />
+        <div>
+            <h3>Places to stay</h3>
+            <div style={formDisplayStyle} className='places-to-stay-form'>
+                <form onSubmit={handelSubmit}>
+                    <div className='form-element'>
+                        <label htmlFor='location'>location</label>
+                        <input
+                            type='text'
+                            name='location'
+                            placeholder='Where are you going?'
+                            value={formData.location}
+                            onChange={handleChange} />
 
-            </div>
-            <div className='form-element'>
-                <label htmlFor='checkIn'>Check in</label>
-                <input
-                    type='date'
-                    name='checkIn'
-                    placeholder='Add dates'
-                    value={formData.checkIn}
-                    onChange={handleChange} />
-            </div>
-            <div className='form-element'>
-                <label htmlFor='checkOut'>Check out</label>
-                <input
-                    type='date'
-                    name='checkOut'
-                    placeholder='Add dates'
-                    value={formData.checkOut}
-                    onChange={handleChange} />
-            </div>
+                    </div>
+                    <div className='form-element'>
+                        <label htmlFor='checkIn'>Check in</label>
+                        <input
+                            type='date'
+                            name='checkIn'
+                            placeholder='Add dates'
+                            value={formData.checkIn}
+                            onChange={handleChange} />
+                    </div>
+                    <div className='form-element'>
+                        <label htmlFor='checkOut'>Check out</label>
+                        <input
+                            type='date'
+                            name='checkOut'
+                            placeholder='Add dates'
+                            value={formData.checkOut}
+                            onChange={handleChange} />
+                    </div>
 
-            <div className='form-element'>
-                <label htmlFor='guests'>Guests</label>
-                <input 
-                type='textarea' 
-                name='guests' 
-                placeholder={formData.guests === 0 ? "Add guests" : formData.guests}
-                value={formData.guests} 
-                onChange={handleChange} />
-            </div>
-            
-            <button className='search-button' placeholder='Search'><i class="fa fa-search"><span>Search</span></i></button>
-        </form>
-       <div className='counters-container' style={guest_counter_container_style} >
-               {guest_counter_elements}
-       </div>
-       </div>
+                    <div className='form-element'>
+                        <label htmlFor='guests'>Guests</label>
+                        <input
+                            type='textarea'
+                            name='guests'
+                            placeholder={formData.guests === 0 ? "Add guests" : formData.guests}
+                            value={formData.guests}
+                            onChange={handleChange} />
+                    </div>
 
-       
+                    <button className='search-button' placeholder='Search'><i class="fa fa-search"><span>Search</span></i></button>
+                </form>
+                <div className='counters-container' style={guest_counter_container_style} >
+                    {guest_counter_elements}
+                </div>
+            </div>
+        </div>
+
+
+
     )
 }
